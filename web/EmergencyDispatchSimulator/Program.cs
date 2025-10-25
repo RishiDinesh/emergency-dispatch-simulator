cac44bba2d2e94e59efc16f821e2e550bdf8937e
nic <bolton21@uwindsor.ca>
2025-10-24 22:01:59 -0400

    add MudBlazor

--- web/EmergencyDispatchSimulator/Program.cs
+++ web/EmergencyDispatchSimulator/Program.cs
@@ -1,10 +1,14 @@
 using EmergencyDispatchSimulator.Components;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+// Add MudBlazor
+builder.Services.AddMudServices();
 
 var app = builder.Build();
 
