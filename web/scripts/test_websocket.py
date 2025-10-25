e35c6d1059bd63604ac566dd1571fc92274725de
nic <bolton21@uwindsor.ca>
2025-10-25 18:51:38 -0400

    base audio interaction implementation

--- web/scripts/test_websocket.py
+++ web/scripts/test_websocket.py
@@ -6,8 +6,7 @@ app = FastAPI()
 
 with open('./test-2mb.wav', 'rb') as infile:
     bin_file_data = infile.read()
-    b64Str2Mb = base64.b64encode(bin_file_data).decode('utf-8')
-
+    bytes2Mb = bin_file_data
 
 
 @app.websocket("/ws")
@@ -15,5 +14,5 @@ async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     while True:
         data = await websocket.receive_text()
-        print(f'received: {data}')
-        await websocket.send_text(b64Str2Mb)
+        # print(f'received: {data}')
+        await websocket.send_bytes(bytes2Mb)
