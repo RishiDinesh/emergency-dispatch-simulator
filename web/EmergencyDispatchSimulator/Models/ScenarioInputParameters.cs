94d48a20f9e8159b03edb9f64785ed15408e7dea
nic <bolton21@uwindsor.ca>
2025-10-25 11:32:27 -0400

    add ScenarioInputParameters

---
+++ web/EmergencyDispatchSimulator/Models/ScenarioInputParameters.cs
@@ -0,0 +1,42 @@
+namespace  EmergencyDispatchSimulator.Models;
+
+public record ScenarioInputParameters
+{
+    public string? IncidentCategory { get; set; }
+    public string? LocationType { get; set; }
+    public string? Gender { get; set; }
+    public string? Language { get; set; }
+    public string? Emotion { get; set; }
+
+    public string? GetParameter(string name) => name switch
+    {
+        ScenarioInputOptions.GroupCategory => IncidentCategory,
+        ScenarioInputOptions.GroupLocation => LocationType,
+        ScenarioInputOptions.GroupGender => Gender,
+        ScenarioInputOptions.GroupLanguage => Language,
+        ScenarioInputOptions.Emotion => Emotion,
+        _ => null
+    };
+
+    public void SetParameter(string name, string value)
+    {
+        switch (name)
+        {
+            case ScenarioInputOptions.GroupCategory:
+                IncidentCategory = value;
+                break;
+            case ScenarioInputOptions.GroupLocation:
+                LocationType = value;
+                break;
+            case ScenarioInputOptions.GroupGender:
+                Gender = value;
+                break;
+            case ScenarioInputOptions.GroupLanguage:
+                Language = value;
+                break;
+            case ScenarioInputOptions.Emotion:
+                Emotion = value;
+                break;
+        }
+    }
+}
