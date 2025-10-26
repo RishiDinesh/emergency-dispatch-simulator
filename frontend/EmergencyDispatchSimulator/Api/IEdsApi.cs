d35cb872d291113fad867fc7f99bfbdc0cfddb85
nic <bolton21@uwindsor.ca>
2025-10-25 22:07:54 -0400

    add docker-compose for backend and frontend

--- frontend/EmergencyDispatchSimulator/Api/IEdsApi.cs
+++ frontend/EmergencyDispatchSimulator/Api/IEdsApi.cs
@@ -9,8 +9,26 @@ public interface IEdsApi
 
 public class EdsApi : IEdsApi
 {
+
+    private readonly HttpClient _http;
+    
+    public EdsApi()
+    {
+        var apiUrl = Environment.GetEnvironmentVariable("BACKEND_API_URL")
+                             ?? "http://localhost:8000";
+        Console.WriteLine($"Api url set to {apiUrl}");
+        
+        _http = new()
+        {
+            BaseAddress = new Uri(apiUrl)
+        };
+    }
+    
+    
     public async Task CreateDispatchSimulation(ScenarioInputParameters parameters)
     {
         // TODO
+        var response = await _http.GetAsync("/");
+        Console.WriteLine($"DONE with response {response.StatusCode}");
     }
 }
