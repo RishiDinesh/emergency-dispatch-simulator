e82b2c8382623afb213bacb1de81625980ae65d0
Adam Cormier <adamcorm16@gmail.com>
2025-10-26 01:14:38 -0400

    fixed analyze call

--- backend/page_3/analyze_call.py
+++ backend/page_3/analyze_call.py
@@ -9,12 +9,15 @@ class AnalyzeCall(object):
 
     def generate_summary(self, message_index=-1):
         if message_index == -1:
-            output_questions = "/data/summary_prompt.txt"
+            output_questions = "/backend/data/summary_prompt.txt"
         else: 
-            output_questions = "/data/individual_summary_prompt.txt"
+            output_questions = "/backend/data/individual_summary_prompt.txt"
 
+        system_content = "You are a 911 call trainer evaluating operator performance. Here is a conversation."
 
-        system_content = "You are a 911 call trainer evaluating operator performance."
+        guidelines = "/backend/data/operator_evaluation_guidelines.txt"
+
+        followup_questions = "Organize your feedback into three bulletpoints for things I did well and three bulletpoints for things I did poorly. Focus on how I said what I said as well as what I said. Don't be too verbose."
 
         if message_index == -1:
             message_index = len(self.call_logs) - 1
@@ -22,23 +25,24 @@ class AnalyzeCall(object):
         messages = [Message(role = "system", content = system_content)]
         for i in range(message_index+1):
             messages.append(
-                {
-                    Message(
-                        role = "user", 
-                        content = [
-                            MessageContent(
-                                type = "input_audio",
-                                input_audio = InputAudio(
-                                    data = self.call_logs[i].audio,
-                                    format = 'wav',
-                                )
+                Message(
+                    role = "user", 
+                    content = [
+                        MessageContent(
+                            type = "input_audio",
+                            input_audio = InputAudio(
+                                data = self.call_logs[i].audio,
+                                format = 'wav',
                             )
-                        ]
-                    )
-                }
+                        )
+                    ]
+                )
             )
         
-        messages.append({"role": "user", "content": output_questions})
+        messages.append(Message(role = "user", content = output_questions))
+        if message_index == len(self.call_logs)-1:
+            messages.append(Message(role = "user", content = followup_questions))
+            messages.append(Message(role = "user", content = guidelines))
 
         return self.llm.get_text_from_speech(messages = messages)
     
--- test_2.py
+++ test_2.py
@@ -1,8 +1,11 @@
 import base64
 import asyncio
 from backend.simulator import Simulator
-from backend._types import UserParams
+from backend._types import UserParams, Log
 import logging
+
+from backend.page_3.analyze_call import AnalyzeCall
+
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(name)s: %(message)s"
@@ -22,32 +25,63 @@ import logging
 logger = logging.getLogger(__name__)
 async def main():
     logger.info("hello")
-    input_queue = asyncio.Queue()
-    output_queue = asyncio.Queue()
-    simulator = Simulator(
-        input_queue=input_queue,
-        output_queue=output_queue,
-        user_params = UserParams(
-            incident = "house fire",
-            location = "house",
-            emotion = "fear_1",
-            gender = "male",
-            language = "english"
-        )
-    )
+    # input_queue = asyncio.Queue()
+    # output_queue = asyncio.Queue()
+    # simulator = Simulator(
+    #     input_queue=input_queue,
+    #     output_queue=output_queue,
+    #     user_params = UserParams(
+    #         incident = "sexual_assault",
+    #         location = "transit",
+    #         emotion = "angry_1",
+    #         gender = "female",
+    #         language = "english"
+    #     )
+    # )
 
-    asyncio.create_task(simulator.run_simulation())
-    user_messages_list = [
-        "usermsg1.wav",
-        "usermsg2.wav",
-        "usermsg3.wav"
-    ]
-    for i, msg in enumerate(user_messages_list):
-        user_msg = file_to_base64(msg)
-        await input_queue.put({"data": user_msg})
-        item = await output_queue.get()
-        response = item["data"]
-        base64_to_file(response, f"ass_msg_{i}.wav")
+    # asyncio.create_task(simulator.run_simulation())
+    # user_messages_list = [
+    #     "bad_assistant_1.wav",
+    #     "bad_assistant_2.wav",
+    #     "bad_assistant_3.wav"
+    # ]
+
+    
+
+    # for i, msg in enumerate(user_messages_list):
+    #     user_msg = file_to_base64(msg)
+    #     await input_queue.put({"data": user_msg})
+    #     item = await output_queue.get()
+    #     response = item["data"]
+    #     base64_to_file(response, f"ass_msg_assault_{i}.wav")
+    
+
+    caller_messages = []
+    
+    for i in range(0,3):
+        with open(f"ass_msg_assault_{i}.wav", "rb") as audio_file:
+            b64 = base64.b64encode(audio_file.read()).decode("utf-8")
+            log = Log(role='user',timestamp=0,audio=b64,transcription="",emotion="")
+            caller_messages.append(log)
+
+    assistant_messages = []
+    
+    for i in range(1,4):
+        with open(f"bad_assistant_{i}.wav", "rb") as audio_file:
+            b64 = base64.b64encode(audio_file.read()).decode("utf-8")
+            log = Log(role='assistant',timestamp=0,audio=b64,transcription="",emotion="")
+            assistant_messages.append(log)
+        
+
+    combined_messages = [item for pair in zip(assistant_messages, caller_messages) for item in pair]
+
+    analyzer = AnalyzeCall(combined_messages)
+
+    summary = analyzer.generate_summary(message_index=0)
+
+    logger.info("TEST " + summary)
+
+
 
 if __name__ == "__main__":
     asyncio.run(main())
